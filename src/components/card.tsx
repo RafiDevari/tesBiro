df5980c0ac61bdd38808039e42ddb45264cb5088
RafiDevari <rafidevari2004@gmail.com>
2025-02-24 19:42:23 +0700

    bikin component title bunga

---
+++ src/components/card.tsx
@@ -0,0 +1,16 @@
+interface CardProps {
+  title: string;
+}
+
+export default function Card({ title }: CardProps) {
+    return (
+      <div className="bg-green-800 inline-flex w-fit items-center">
+        <div>
+          <img src="/images/bunga.png" alt="Simple Image" width="40" height="40" />
+        </div>
+        <div>
+          <h1 className="mx-4 text-white font-semibold text-xl font-sans">{title}</h1>
+        </div>
+      </div>
+    );
+  }
---
+++ src/components/testComp.tsx
@@ -0,0 +1,8 @@
+export default function TestComp() {
+    return (
+      <div>
+        <h2>Hello from MyComponent!</h2>
+        <p>This is a reusable component.</p>
+      </div>
+    );
+  }
